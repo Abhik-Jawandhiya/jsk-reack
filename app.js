98f89eb8094f730c63390f96fe5753bc6d4254ef
Abhik Jawandhiya <abhik@tejuri.com>
2024-08-06 13:07:04 +0400

    functional component

--- app.js
+++ app.js
@@ -5,25 +5,63 @@ import ReactDOM from "react-dom/client";
 //React.createElement => ReactEleemnt - JS Object => HTMLElement(render)
 const heading = React.createElement("h1", {id: "heading"}, "JSK React");
 
-console.log(heading);
+//React Component
+//Class Based Component - Old way
+//Functional Components - New way
 
-//JSX - Not HTML in javascript (It is HTML like syntax)
-//JSX code is transpiled before it reaches the JS engine - This is done by PARCEL with the help of BABEL(JavaScript Compiler/ Transpiler)
-//JSX Code -> transpiled to createElement React.createElement => ReactEleemnt - JS Object => HTMLElement(render)
-//Various arributes of JSX - img tag
-//JSX - HTML Attributes are given as camel case ()
+//React Functional Component
+// const HeadingComponent = () => {
+//     return <h1 className="heading">JSk React Functional Component</h1>;
+// };
 
-const jsxHeading = (
-    <h1 id="heading" className="heading">
-        JSK React using JSX
+//const HeadingComponent = () => <h1 className="heading">JSk React Functional Component</h1>;
+
+//React Element
+const heading1 = ( 
+    <h1 className="heading">J
+        Jsk React Element
     </h1>
 );
-console.log(jsxHeading);
+
+//Title Component
+// const TitleComponent = () => ( 
+//     <h1 className="title-component">
+//          Title Component
+//     </h1>
+// );
+
+// const TitleComponent = function() {
+//     return ( 
+//         <h1 className="title-component">
+//              Title Component
+//         </h1>
+//     );
+// };
+
+const elem = <span>React element elem </span>
 
 
-//React Component
+
+const number = 1000;
+
+const HeadingComponent = () => (
+    <div class="container">
+        <h1 className="heading">JSk React Functional Component</h1>
+    </div>
+);
+
+const title = (
+    <h1 className="title-component">   
+        {elem}     
+        Title Element
+        <HeadingComponent />
+    </h1>
+);
+
+
+//What is component composition -> composing title component in heading component is component composition
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(title);
